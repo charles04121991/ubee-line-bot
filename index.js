7702c4c0f8642604dd65e0fddeffeb80d7837703
charles04121991 <as25252159@yahoo.com.tw>
2026-04-21 11:40:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -452,7 +452,7 @@ function createMainMenuQuickReply() {
 }
 
 function createOrderMenuQuickReply() {
-  return createQuickReplyMessage('請選擇您要使用的服務', [
+  return createQuickReplyMessage('請選擇服務🚀', [
     qrPostback('建立任務', 'action=create', '建立任務'),
     qrPostback('立即估價', 'action=quote', '立即估價'),
     qrMessage('計費說明', '計費說明'),
@@ -462,7 +462,7 @@ function createOrderMenuQuickReply() {
 }
 
 function createEnterpriseMenuQuickReply() {
-  return createQuickReplyMessage('請選擇您需要的服務項目', [
+  return createQuickReplyMessage('請選擇您需要的服務項目💼', [
     qrUri('企業合作申請', BUSINESS_FORM),
     qrMessage('企業服務說明', '企業服務說明'),
     qrMessage('服務區域', '服務區域'),
@@ -471,7 +471,7 @@ function createEnterpriseMenuQuickReply() {
 }
 
 function createMyMenuQuickReply() {
-  return createQuickReplyMessage('請選擇您要查看的項目', [
+  return createQuickReplyMessage('請選擇要查看的內容📦', [
     qrMessage('服務說明', '服務說明'),
     qrMessage('常見問題', '常見問題'),
     qrUri('加入夥伴', PARTNER_FORM),
